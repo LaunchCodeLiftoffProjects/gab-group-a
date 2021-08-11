12b4e4a5bc61e9a086a46a3e1f41c3a005a666f3
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-10 22:39:43 -0500

    added icons to NavBar

--- client/helping-hands/src/NavBar.js
+++ client/helping-hands/src/NavBar.js
@@ -4,6 +4,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css"
 import HomeIcon from '@material-ui/icons/Home';
+import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 export default function NavBar() {
     return (
@@ -14,16 +17,16 @@ export default function NavBar() {
                 <Link to="/"><Button variant="contained" color="primary" className="btn"><HomeIcon /></Button></Link> 
             </span>
             <span className="col-2" align="center">
-                <Link to="/my-haves"><Button className="btn" variant="contained" color="primary">My Haves </Button></Link>
+                <Link to="/my-haves"><Button className="btn" variant="contained" color="primary"><FormatListBulletedIcon /> My Haves </Button></Link>
             </span>
-            <span className="col-2">
-                <Link to="/my-needs"><Button  variant="contained" color="primary" className="btn">My Needs</Button></Link>   
+            <span className="col-2" align="center">
+                <Link to="/my-needs"><Button  variant="contained" color="primary" className="btn"><FormatListBulletedIcon /> My Needs</Button></Link>   
             </span>
-            <span className="col-2" align="right">
-                <Link to="/search"><Button className="btn" variant="contained" color="primary">Search </Button></Link>
+            <span className="col-2" align="center">
+                <Link to="/search"><Button className="btn" variant="contained" color="primary"><SearchIcon />Search </Button></Link>
             </span>
             <span className="col-3" align="right">
-                <Link to="/profile"><Button variant="contained" color="primary" className="btn">Profile</Button></Link>
+                <Link to="/profile"><Button variant="contained" color="primary" className="btn"><AccountCircleIcon /></Button></Link>
             </span>
             </span>
         </nav>
