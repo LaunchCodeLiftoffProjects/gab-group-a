32f86a2fbdbea2a2f73136b096d9980f2eb3ee8a
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-10 22:50:26 -0500

    added edit icons to Profile

--- client/helping-hands/src/Profile.js
+++ client/helping-hands/src/Profile.js
@@ -4,6 +4,8 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import EditIcon from '@material-ui/icons/Edit';
 import "./Profile.css";
 
 export default function Profile() {
@@ -53,7 +55,10 @@ export default function Profile() {
                         <p><CardActionArea>
                             <Card>
                                 <CardContent>
-                                    <Typography variant="h6">Haves</Typography>
+                                    <span className="row">
+                                        <Typography className="col-11" variant="h6">Haves</Typography>
+                                        <Button className = "col-1" align="right"><EditIcon /></Button>
+                                    </span>
                                     <Typography variant = "subtitle3" >
                                         <ul >
                                             {userPlaceholder.has.map((item) => {
@@ -70,7 +75,10 @@ export default function Profile() {
                     <CardActionArea>
                             <Card>
                                 <CardContent>
-                                    <Typography variant="h6">Needs</Typography>
+                                    <span className="row">
+                                        <Typography className="col-11" variant="h6">Needs</Typography>
+                                        <Button className = "col-1" align="right"><EditIcon /></Button>
+                                    </span>
                                     <Typography variant = "subtitle3" >
                                         <ul >
                                             {userPlaceholder.needs.map((item) => {
