7e69de76f63d74291eb861e2db250f2f1ab51a13
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-12 10:18:46 -0500

    added UserController

---
+++ server/Prototype-backend-for-Helping-Hands/src/main/java/com/helpinghands/backendPrototype/Controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.helpinghands.backendPrototype.Controllers;
+
+import com.helpinghands.backendPrototype.Data.UserRepository;
+import com.helpinghands.backendPrototype.Models.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    List<User> all() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    User one(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(); //TODO create a UserNotFound exception to throw here.
+    }
+
+}
