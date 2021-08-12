3360726c308df4e4041992b41d6bb08373ef539f
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-12 12:43:01 -0500

    backend migrated over to MySQL DB. Username and pass set to enviro vars.

--- server/helping-hands-backend/src/main/java/com/helpinghands/backendPrototype/Controllers/UserController.java
+++ server/helping-hands-backend/src/main/java/com/helpinghands/backendPrototype/Controllers/UserController.java
@@ -2,6 +2,7 @@ package com.helpinghands.backendPrototype.Controllers;
 
 import com.helpinghands.backendPrototype.Data.UserRepository;
 import com.helpinghands.backendPrototype.Models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,16 @@ import java.util.List;
 
 @RestController
 public class UserController {
+    @Autowired
     private final UserRepository userRepository;
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+
     @GetMapping("/users")
-    List<User> all() {
+    Iterable<User> all() {
         return userRepository.findAll();
     }
 
--- server/helping-hands-backend/src/main/java/com/helpinghands/backendPrototype/Data/UserRepository.java
+++ server/helping-hands-backend/src/main/java/com/helpinghands/backendPrototype/Data/UserRepository.java
@@ -1,8 +1,9 @@
 package com.helpinghands.backendPrototype.Data;
 
 import com.helpinghands.backendPrototype.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+
+public interface UserRepository extends CrudRepository<User, Long> {
 }
 
