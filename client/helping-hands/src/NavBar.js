324774a6b843ed06aedb92ecccb5e1dc98821e11
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-13 14:44:05 -0500

    added search bar and useStyles hooks to navbar

--- client/helping-hands/src/NavBar.js
+++ client/helping-hands/src/NavBar.js
@@ -10,26 +10,107 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { AppBar, Toolbar } from '@material-ui/core';
 import NavBarMenu from './NavBarMenu';
 import { IconButton } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+import InputBase from '@material-ui/core/InputBase';
 
 
 export default function NavBar() {
+
+    const useStyles = makeStyles((theme) => ({
+        grow: {
+          flexGrow: 1,
+        },
+        menuButton: {
+          marginRight: theme.spacing(2),
+        },
+        title: {
+          display: 'none',
+          [theme.breakpoints.up('sm')]: {
+            display: 'block',
+          },
+        },
+        search: {
+          position: 'relative',
+          borderRadius: theme.shape.borderRadius,
+        //   backgroundColor: alpha(theme.palette.common.white, 0.15),
+        //   '&:hover': {
+        //     backgroundColor: alpha(theme.palette.common.white, 0.25),
+        //   },
+          marginRight: theme.spacing(2),
+          marginLeft: 0,
+          width: '100%',
+          [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+          },
+        },
+        searchIcon: {
+          padding: theme.spacing(0, 2),
+          height: '100%',
+          position: 'absolute',
+          pointerEvents: 'none',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        inputRoot: {
+          color: 'inherit',
+        },
+        inputInput: {
+          padding: theme.spacing(1, 1, 1, 0),
+          // vertical padding + font size from searchIcon
+          paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+          transition: theme.transitions.create('width'),
+          width: '100%',
+          [theme.breakpoints.up('md')]: {
+            width: '20ch',
+          },
+        },
+        sectionDesktop: {
+          display: 'none',
+          [theme.breakpoints.up('md')]: {
+            display: 'flex',
+          },
+        },
+        sectionMobile: {
+          display: 'flex',
+          [theme.breakpoints.up('md')]: {
+            display: 'none',
+          },
+        },
+      }));
+      
+    const classes = useStyles();
+
     return (
     <Paper>    
         <AppBar align="center">
             <Toolbar>
-            <div className="container">
-            <div className="row">
-                <div className="col-1">
+            
+                
                     <Link to="/"><IconButton edge="start" color="inherit" aria-label="home"><HomeIcon /></IconButton></Link>
-                </div>
-                <div className = "col-4" align="left">
+                
+                
                     <NavBarMenu />
-                </div>
-                <div className="col-7" align="right">
+                
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                            <SearchIcon />
+                            </div>
+                        <InputBase
+                        placeholder="Search…"
+                        classes={{
+                            root: classes.inputRoot,
+                            input: classes.inputInput,
+                        }}
+                        inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </div>
+                    <div className={classes.grow}></div>
+                
                     <Link to="/profile"><IconButton align="right" edge="start" color="inherit" aria-label="profile"><AccountCircleIcon /></IconButton></Link>
-                </div>
-            </div> 
-            </div>
+                
+            
             </Toolbar>
         </AppBar>
     </Paper>
