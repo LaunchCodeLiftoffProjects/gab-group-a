03c87dad721cd3d86c57c71ca7635d094e26b41a
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-14 12:17:16 -0500

    updated profile to use user's email.

--- client/helping-hands/src/Profile.js
+++ client/helping-hands/src/Profile.js
@@ -76,7 +76,7 @@ export default function Profile({id}) {
                                 <div className="container">
                                     <span className="row"><Avatar className="col-1">{user.name[0]}</Avatar><Typography className="col-8" variant="h6">{user.name}</Typography></span>
                                     <span className="row"><br /><Typography variant="subtitle1">{user.location.name}</Typography></span>
-                                    <span className="row"><Typography variant="subtitle1">{userPlaceholder.email}</Typography></span>
+                                    <span className="row"><Typography variant="subtitle1">{user.email}</Typography></span>
                                 </div>
                                 </CardContent>
                             </Card>
