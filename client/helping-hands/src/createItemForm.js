39fd1fbe632956c7f8f8ab65f5e94d3ad995e403
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-17 13:09:27 -0500

    added axios, tried making POST with axios, still no go.

---
+++ client/helping-hands/src/createItemForm.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import Paper from '@material-ui/core/Paper';
+import Card from "@material-ui/core/Card";
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { TextField } from "@material-ui/core";
+import { InputLabel } from "@material-ui/core";
+import { Select } from "@material-ui/core";
+import { MenuItem } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import { FormControl } from "@material-ui/core";
+import { FormControlLabel } from "@material-ui/core";
+import axios from 'axios';
+
+export default function CreateItemForm() {
+
+    const [values, setValues] = useState({
+        name: "",
+        description: "",
+        itemCategory: {},
+        usersWhoHave: [],
+        usersWhoNeed: [],
+        amtHave: 0,
+        amtNeed: 0
+    })    
+
+    const itemLiteral = {
+        name: "Beans",
+        description: "Dried beans:",
+        itemCategory: {},
+        usersWhoHave: [],
+        usersWoNeed: [],
+        amtHave: 0,
+        amtNeed: 0
+    }
+
+    const createItem = async (item) => {
+        const response = await axios.post("http://localhost:8080/items", itemLiteral)
+    }
+
+    useEffect(() => {
+        createItem();
+    })
+
+    return(
+        <div>
+            <Paper>
+                <CardActionArea>
+                    <Card>
+                        <CardContent>
+                            <FormControl>
+                                <TextField>
+
+                                </TextField>
+                            </FormControl>
+                        </CardContent>
+                    </Card>
+                </CardActionArea>
+            </Paper>
+        </div>
+    )
+  
+}
