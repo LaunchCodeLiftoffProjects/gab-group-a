f1e38f40bce6ef953b46625612be908131f1eb4f
Daryn <dhaffer1991@gmail.com>
2021-08-17 13:06:25 -0500

    Added logic to user model, authentication controller, and form DTOs

---
+++ server/helping-hands-backend/src/main/java/com/helpinghands/backendPrototype/Controllers/AuthenticationController.java
@@ -0,0 +1,50 @@
+package com.helpinghands.backendPrototype.Controllers;
+
+import com.helpinghands.backendPrototype.Data.UserRepository;
+import com.helpinghands.backendPrototype.Models.User;
+import com.helpinghands.backendPrototype.Models.dto.RegisterFormDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Controller
+public class AuthenticationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    // ---- Stores and retrieves login status of a user in a session. ---
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+    }
+    // -----------------------------------------------------------------
+
+    @GetMapping("/register")
+    public String displayRegistrationForm() {
+
+
+        return "register";
+    }
+
+}
--- server/helping-hands-backend/src/main/java/com/helpinghands/backendPrototype/Data/UserRepository.java
+++ server/helping-hands-backend/src/main/java/com/helpinghands/backendPrototype/Data/UserRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String name);
+
 }
