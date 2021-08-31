643359470f304b02eeb1fcd5d0e1c15b49c93df3
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-31 10:40:46 -0500

    init file struc for user search functions

---
+++ server/helping-hands-backend/src/main/java/com/helpinghands/backendPrototype/Controllers/SearchControllers/UserSearchController.java
@@ -0,0 +1,9 @@
+package com.helpinghands.backendPrototype.Controllers.SearchControllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class UserSearchController {
+}
