34f7ef0fabec4529717ca1dad7e1c66f2994169d
C. Gremaud <colyn.gremaud@gmail.com>
2021-09-14 10:21:59 -0500

    added more fields to results state obj, added findCategoryMatches method

--- client/helping-hands/src/item/ViewItemMatches.js
+++ client/helping-hands/src/item/ViewItemMatches.js
@@ -6,18 +6,46 @@ import { findItemByCategory, findItemByName } from "../search/search-item";
 export default function ViewItemMatches() {
     const [results, setResults] = useState({
         exactMatches: [],
-        categoryMatches: []
+        categoryMatches: [],
+        usersLocation: [],
+        allUsers:[]
     });
-    const [loaded, setLoaded] = useState(false);
+    const [loaded, setLoaded] = useState({
+        exactResults: false,
+        categoryResults: false
+    });
     const [error, setError] = useState();
     const [user, setUser] = useState();
 
-    const findExactMatches = async (user) => {
+    const findExactMatches = async user => {
         let result = [];
         for (item in user.needsItems) {
-            result.push(await findItemByName(item.name))
+            result.push(...await findItemByName(item.name))
         }
+        setResults(prevResults => ({
+            ...prevResults,
+            exactMatches: result
+        }))
+        setLoaded(val => ({
+            ...val,
+            exactLoaded: true
+        }))
 
+    }
+
+    const findCategoryMatches = async user => {
+        let result = []
+        for (item in user.needsItems) {
+            result.push(...await findItemByCategory(item.itemCategory.name))
+        }
+        setResults(prevResults => ({
+            ...prevResults,
+            categoryMatches: result
+        }))
+        setLoaded(val => ({
+            ...val,
+            categoryResults: true
+        }))
     }
 
     const findCategoryMatches = async (user) => {
