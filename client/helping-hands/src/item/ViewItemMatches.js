bbc376b010172fd3d3317c0f9a1b869f89809df6
C. Gremaud <colyn.gremaud@gmail.com>
2021-09-15 15:13:37 -0500

    added css to viewitemmatches, started writing jsx

--- client/helping-hands/src/item/ViewItemMatches.js
+++ client/helping-hands/src/item/ViewItemMatches.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Paper, Card, CardContent, Typography } from "@material-ui/core";
+import { Paper, Card, CardContent, Typography, ListItem } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import { findItemByCategory, findItemByName } from "../search/search-item";
+import "./ViewItemMatches.css"
 
 export default function ViewItemMatches() {
     const [results, setResults] = useState({
         exactMatches: [],
         categoryMatches: [],
-        usersLocation: [],
-        allUsers:[]
+        // usersLocation: [],
+        // allUsers:[] //instead of this just find all of the users in the usersWhoHave of each item, and match location ids. 
     });
     const [loaded, setLoaded] = useState({
         exactResults: false,
@@ -37,6 +38,7 @@ export default function ViewItemMatches() {
         let result = []
         for (item in user.needsItems) {
             result.push(...await findItemByCategory(item.itemCategory.name))
+            
         }
         setResults(prevResults => ({
             ...prevResults,
@@ -47,13 +49,39 @@ export default function ViewItemMatches() {
             categoryResults: true
         }))
     }
-
-    const findCategoryMatches = async (user) => {
+    //Question: Will I have to manually send along the session ID here? or will it be automatically pkgd with the request? 
+    const fetchCurrentUser = async () => {
+        const result;
+        result = await fetch("http://localhost:8080/current-user")
+        setUser(await result.json());
 
     }
 
-    useEffect(user => {
-
+    useEffect(async () => {
+        await fetchCurrentUser();
+        await findExactMatches(user);
+        await findCategoryMatches(user);
     }, [])
+
+    if (!loaded.categoryResults || !loaded.exactResults) {
+        return <div>Loading . . . </div>
+    } else if (error) {
+        return <div>{error.message}</div>
+    } else {
+        return (
+            <Paper className="match-paper">
+                <Typography variant="h4">Users matching your needed Items</Typography>
+                <List>
+                    {results.exactMatches.map((result, i) => {
+                        return (
+                            <ListItem>
+                                Item Name: {result.name}
+                            </ListItem>
+                        )
+                    })}
+                </List>
+            </Paper>
+        )
+    }
 
 }
